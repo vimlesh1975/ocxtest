bd892f5f8a8483ac6857ba1bfd3f91fb379ea177
VIMLESH KUMAR <vimlesh1975@hotmail.com>
2023-04-06 21:50:08 +0530

    aa

--- ocxtest/Form1.Designer.vb
+++ ocxtest/Form1.Designer.vb
@@ -23,28 +23,23 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
-        Me.Panel1 = New System.Windows.Forms.Panel()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.TextBox4 = New System.Windows.Forms.TextBox()
         Me.TextBox5 = New System.Windows.Forms.TextBox()
         Me.Button4 = New System.Windows.Forms.Button()
+        Me.flashControl = New AxShockwaveFlashObjects.AxShockwaveFlash()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.flashControl, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'Panel1
-        '
-        Me.Panel1.Location = New System.Drawing.Point(1, 1)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.Size = New System.Drawing.Size(400, 300)
-        Me.Panel1.TabIndex = 5
-        Me.Panel1.Visible = False
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(373, 349)
+        Me.Button3.Location = New System.Drawing.Point(373, 319)
         Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.Size = New System.Drawing.Size(135, 23)
         Me.Button3.TabIndex = 10
-        Me.Button3.Text = "Button3"
+        Me.Button3.Text = "Load template with data"
         Me.Button3.UseVisualStyleBackColor = True
         '
         'TextBox4
@@ -52,47 +47,79 @@ Partial Class Form1
         Me.TextBox4.Location = New System.Drawing.Point(12, 313)
         Me.TextBox4.Multiline = True
         Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(345, 130)
+        Me.TextBox4.Size = New System.Drawing.Size(345, 29)
         Me.TextBox4.TabIndex = 11
-        Me.TextBox4.Text = resources.GetString("TextBox4.Text")
+        Me.TextBox4.Text = "Live from Mumbai"
         '
         'TextBox5
         '
-        Me.TextBox5.Location = New System.Drawing.Point(1, 451)
+        Me.TextBox5.Location = New System.Drawing.Point(12, 348)
         Me.TextBox5.Multiline = True
         Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(356, 124)
+        Me.TextBox5.Size = New System.Drawing.Size(356, 26)
         Me.TextBox5.TabIndex = 13
-        Me.TextBox5.Text = resources.GetString("TextBox5.Text")
+        Me.TextBox5.Text = "DDK MUmbai"
         '
         'Button4
         '
-        Me.Button4.Location = New System.Drawing.Point(373, 487)
+        Me.Button4.Location = New System.Drawing.Point(374, 346)
         Me.Button4.Name = "Button4"
         Me.Button4.Size = New System.Drawing.Size(75, 23)
         Me.Button4.TabIndex = 14
-        Me.Button4.Text = "Button4"
+        Me.Button4.Text = "Update"
         Me.Button4.UseVisualStyleBackColor = True
+        '
+        'flashControl
+        '
+        Me.flashControl.Enabled = True
+        Me.flashControl.Location = New System.Drawing.Point(1, 12)
+        Me.flashControl.Name = "flashControl"
+        Me.flashControl.OcxState = CType(resources.GetObject("flashControl.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.flashControl.Size = New System.Drawing.Size(471, 282)
+        Me.flashControl.TabIndex = 15
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 400)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "Label1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(374, 375)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 17
+        Me.Button1.Text = "Stop"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(484, 589)
+        Me.ClientSize = New System.Drawing.Size(520, 441)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.flashControl)
         Me.Controls.Add(Me.Button4)
         Me.Controls.Add(Me.TextBox5)
         Me.Controls.Add(Me.TextBox4)
         Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Panel1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.flashControl, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Panel1 As Panel
     Friend WithEvents Button3 As Button
     Friend WithEvents TextBox4 As TextBox
     Friend WithEvents TextBox5 As TextBox
     Friend WithEvents Button4 As Button
+    Friend WithEvents flashControl As AxShockwaveFlashObjects.AxShockwaveFlash
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
 End Class
--- ocxtest/Form1.vb
+++ ocxtest/Form1.vb
@@ -1,37 +1,43 @@
 ﻿
-Imports System.Windows.Forms.VisualStyles.VisualStyleElement
-Imports AxShockwaveFlashObjects
 Imports Svt.Caspar
-Imports Svt.Network
-
 Public Class Form1
-    Dim flashControl As AxShockwaveFlash
     Dim CasparCGDataCollection As New CasparCGDataCollection
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        flashControl = New AxShockwaveFlash()
-        flashControl.Location = New Point(0, 0)
-        flashControl.Size = New Size(400, 300)
-
-        Me.Controls.Add(flashControl)
-
+        'Dim aa As String = "file:///" + Application.StartupPath.Replace("\", "/") + "/cg20.fth.pal"
+        'Console.WriteLine(aa)
         flashControl.Movie = "file:///" + "c:/casparcg/cg20.fth.pal"
-
-
     End Sub
 
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-
-
-        Dim xmlString = TextBox4.Text.Replace("""", "'")
-            flashControl.CallFunction(xmlString)
-
-
+        CasparCGDataCollection.SetData("f0", TextBox4.Text)
+        CasparCGDataCollection.SetData("f1", "E.A. DDK Mumbai")
+        Dim str = "<invoke name='Add' returntype='xml'><arguments><number>8</number><string>CMP/CG_TEMPLATE/TOP_LEFT.ft</string><true/><string></string><string><![CDATA[" + CasparCGDataCollection.ToXml + "]]></string></arguments></invoke>"
+        Try
+            Label1.Text = flashControl.CallFunction(str)
+        Catch ex As Exception
+            Label1.Text = ex.ToString()
+        End Try
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        Dim xmlString = TextBox5.Text.Replace("""", "'")
-        flashControl.CallFunction(xmlString)
+        CasparCGDataCollection.SetData("f0", TextBox5.Text)
+        CasparCGDataCollection.SetData("f1", "E.A. DDK Mumbai")
+        Dim str = "<invoke name='SetData' returntype='xml'><arguments><array><property id='0'><number>8</number></property></array><string><![CDATA[" + CasparCGDataCollection.ToXml + "]]></string></arguments></invoke>"
+        Try
+            Label1.Text = flashControl.CallFunction(str)
+        Catch ex As Exception
+            Label1.Text = ex.ToString()
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim str = "<invoke name='Stop' returntype='xml'><arguments><array><property id='0'><number>8</number></property></array><number>0</number></arguments></invoke>"
+        Try
+            Label1.Text = flashControl.CallFunction(str)
+        Catch ex As Exception
+            Label1.Text = ex.ToString()
+        End Try
     End Sub
 End Class
