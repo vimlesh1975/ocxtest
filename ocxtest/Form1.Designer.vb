1aaf51f71cfda8281cd636edeb5e257256c85c2e
vimlesh1975 <vimlesh1975@hotmail.com>
2023-04-06 17:27:17 +0530

    Add project files.

---
+++ ocxtest/Form1.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.Location = New System.Drawing.Point(1, 1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(400, 300)
+        Me.Panel1.TabIndex = 5
+        Me.Panel1.Visible = False
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(373, 349)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Button3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(12, 313)
+        Me.TextBox4.Multiline = True
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(345, 130)
+        Me.TextBox4.TabIndex = 11
+        Me.TextBox4.Text = resources.GetString("TextBox4.Text")
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(1, 451)
+        Me.TextBox5.Multiline = True
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(356, 124)
+        Me.TextBox5.TabIndex = 13
+        Me.TextBox5.Text = resources.GetString("TextBox5.Text")
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(373, 487)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 14
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 589)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button3 As Button
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button4 As Button
+End Class
---
+++ ocxtest/Form1.vb
@@ -0,0 +1,37 @@
+﻿
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Imports AxShockwaveFlashObjects
+Imports Svt.Caspar
+Imports Svt.Network
+
+Public Class Form1
+    Dim flashControl As AxShockwaveFlash
+    Dim CasparCGDataCollection As New CasparCGDataCollection
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        flashControl = New AxShockwaveFlash()
+        flashControl.Location = New Point(0, 0)
+        flashControl.Size = New Size(400, 300)
+
+        Me.Controls.Add(flashControl)
+
+        flashControl.Movie = "file:///" + "c:/casparcg/cg20.fth.pal"
+
+
+    End Sub
+
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+
+
+        Dim xmlString = TextBox4.Text.Replace("""", "'")
+            flashControl.CallFunction(xmlString)
+
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim xmlString = TextBox5.Text.Replace("""", "'")
+        flashControl.CallFunction(xmlString)
+    End Sub
+End Class
